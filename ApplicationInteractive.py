76e2828e0a9fb14ef3c2aa147885b3fd5e370d07
Wen-Hao Zhao <wzhao@asml.com>
2017-05-24 15:20:42 +0800

    test 2

---
+++ ApplicationInteractive.py
@@ -0,0 +1,20 @@
+import pya  #KLayout  Python interface package
+
+import sys
+sys.path.append(r"C:\Users\wzhao\AppData\Local\Continuum\Anaconda3\envs\py34\Lib")  ##Added other Python package, for KLayout v0.24, Python version is 3.4.2
+sys.path.append(r"C:\Users\wzhao\AppData\Local\Continuum\Anaconda3\envs\py34\Lib\site-packages")
+
+import numpy
+
+current_application = pya.Application.instance()
+main_window = current_application.main_window()
+
+layout_view = main_window.current_view()
+
+k = layout_view.each_object_selected() 
+
+#print(help(layout_view))
+#print(help(k))
+print(next(k))
+
+pya.MessageBox.info("2333", "Hello!", pya.MessageBox.Ok) 
